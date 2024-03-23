59741e8beff6959f2c81fdb7b8dfe7497aa66bbe
Poojitha Vempalli <vempallipoojitha12@gmail.com>
2024-03-23 07:28:59 +0000

    changes in the navbar

---
+++ frontend/src/components/user/subcomponents/Navbar.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import { CodeIcon, HamburgetMenuClose, HamburgetMenuOpen } from "./Icons";
+import {auth} from "../../../firebase"
+import { useNavigate } from "react-router-dom";
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+  const navigate = useNavigate();
+  const handleClick = () => setClick(!click);
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      navigate('/'); 
+    } catch (error) {
+      console.error('Logout Error:', error);
+    }
+  };
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/user" className="nav-logo">
+            <span>Public Rest</span>
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/Recommendations"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Recommendations
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/Sorting"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Sorting
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/Qrcode"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Qrcode
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleLogout}>
+                    Logout
+                </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {/* <i className={click ? "fas fa-times" : "fas fa-bars"}></i> */}
+
+            {click ? (
+              <span className="icon">
+                <HamburgetMenuClose />{" "}
+              </span>
+            ) : (
+              <span className="icon">
+                <HamburgetMenuOpen />
+              </span>
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
