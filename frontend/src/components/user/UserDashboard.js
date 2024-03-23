04b84cc4a67babe9ba7dc79bef6d2251064ed35b
Poojitha Vempalli <vempallipoojitha12@gmail.com>
2024-03-23 07:28:38 +0000

    updaing the userdashboard

---
+++ frontend/src/components/user/UserDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './subcomponents/Navbar';
+import Maps from './subcomponents/Maps'
+
+function UserDashboard() {
+  return (
+    <div>
+        <NavBar />
+        <Maps />
+    </div>
+  )
+}
+
+export default UserDashboard
