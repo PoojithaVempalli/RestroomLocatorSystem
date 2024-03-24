03075241cd03cde5c1da6bc5bf8d8fd2c0c719b9
Poojitha Vempalli <89512429+PoojithaVempalli@users.noreply.github.com>
2024-03-25 04:52:50 +0530

    Resolve the issue Mapbox.js #4

--- frontend/src/components/Map/Mapbox.js
+++ frontend/src/components/Map/Mapbox.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import "../Maps.css";
